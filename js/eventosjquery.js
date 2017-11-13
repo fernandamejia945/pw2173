23cde6256b5c91e025834e36345133bc379ecc83
fernandamejia945 <ma_fer_945@hotmail.com>
2017-11-13 15:55:11 -0700

    RandomUser

--- js/eventosjquery.js
+++ js/eventosjquery.js
@@ -8,21 +8,18 @@ var inicia = function(){
   success: function(data) {
   	//alert(data.results[0].name.first+" "+data.results[0].name.last);
     //console.log(data);
-    $("#nombre").html(data.results[0].name.title+" "+data.results[0].name.first+" "+data.results[0].name.last);
+    $("#nombre").html("Nombre: "+data.results[0].name.title+" "+data.results[0].name.first+" "+data.results[0].name.last);
     $("#foto").attr("src",data.results[0].picture.large);
-    $("#genero").html(data.results[0].gender);
-    $("#email").html(data.results[0].email);
-    $("#login").html(data.results[0].login.username+" "+data.results[0].login.password+" "+data.results[0].login.salt);
-    $("#dob").html(data.results[0].dob);
-    $("#registered").html(data.results[0].registered);
-    $("#phone").html(data.results[0].phone);
-    $("#cell").html(data.results[0].cell);
+    $("#genero").html("Genero: "+data.results[0].gender);
+    $("#email").html("Email: "+data.results[0].email);
+    $("#login").html("Usuario: "+data.results[0].login.username+", Contraseña: "+data.results[0].login.password+" "+data.results[0].login.salt);
+    $("#direccion").html("Direccion: Calle "+data.results[0].location.street+", Ciudad: "+data.results[0].location.city+", Estado: "+data.results[0].location.state+", CP: "+data.results[0].location.postcode);
+    $("#phone").html("Teléfono: "+data.results[0].phone);
+    $("#cell").html("Celular: "+data.results[0].cell);
   } 
 }); 
 }
-		//<article id="phone"></article>
-		//<article id="cell"></article>
-		//<article id="id"></article>
+	nuevo();
 	//alert("Lista la pagina");
 	$("#btnNuevo").on("click",nuevo);
 }
