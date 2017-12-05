6bd24af15f3b889b4b3428b3124ed28907620a72
fernandamejia945 <ma_fer_945@hotmail.com>
2017-12-05 16:03:32 -0700

    Usuario con nombre, botones de menu

--- proyecto/php/entrada.php
+++ proyecto/php/entrada.php
@@ -5,13 +5,21 @@
 		$respuesta= false;
 		$usuario= $_POST["usuario"];
 		$clave= md5($_POST["clave"]);
-		$consulta="select ncontrol, clave from alumnos where ncontrol='".$usuario."' and clave='".$clave."' limit 1";
+		$consulta="select ncontrol, nombre, clave from alumnos where ncontrol='".$usuario."' and clave='".$clave."' limit 1";
+		//RecordSet = Conjunto de registros
 		//echo $consulta;
 		$resConsulta=mysqli_query($conexion,$consulta);
+		//Cuantos registros trae el RecordSet
+		// Cursos - Posiciòn del registro
+		$nombre= "";
 		if(mysqli_num_rows($resConsulta) > 0){
 			$respuesta = true;
+			if($registro=mysqli_fetch_array($resConsulta))
+				{
+					$nombre = $registro["nombre"];
+				}
 		}
-		$salida = array('respuesta' => $respuesta);
+		$salida = array('respuesta' => $respuesta, 'nombre' => $nombre);
 		print json_encode($salida);
 	}
 	$opc=$_POST["opc"];
