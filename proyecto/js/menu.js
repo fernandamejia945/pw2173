18a9a4bedbbfc64ca0dbaf748d52f2c2d4fc36fb
fernandamejia945 <ma_fer_945@hotmail.com>
2017-12-07 16:06:11 -0700

    buscaAlumno

--- proyecto/js/menu.js
+++ proyecto/js/menu.js
@@ -28,6 +28,32 @@ var iniciaMenu = function(){
 			}
 		});
 }
+	var teclaNcontrol = function(tecla){
+		if (tecla.which == 13) //13=Enter0
+			{
+				var ncontrol=$("#txtNcontrol").val();
+				var parametros="opc=buscaNcontrol"+"&ncontrol="+ncontrol+"&id="+Math.random();
+				$.ajax({
+			url:"php/buscancontrol.php",
+			dataType: 'json',
+			type: "POST", //lo que enviamos
+			data:parametros,
+			success:function(data){
+				if(data.respuesta == true){
+					$("#txtNombre").val(data.nombre);
+					$("#txtCarrera").val(data.carrera);
+					$("#txtClave").val(data.clave);
+				}else{
+					$("#txtNombre").focus();
+				}
+			},
+			error:function(a,b,c){
+					alert("No se pudo conectar al server");
+			}
+		});
+			}
+	}
+	$("#txtNcontrol").on("keypress",teclaNcontrol);
 	$("#btnAlta").on("click",alta);
 	$("#btnAltaAlumno").on("click",altaAlumno);
 }
